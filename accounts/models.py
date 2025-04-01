335f4c5b8f00dbeae1caba041bfb7bb80bb2afd1
PepperShin <peep1996@naver.com>
2025-04-01 11:25:11 +0900

    commit

---
+++ accounts/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+# dev_9
+# User 계정 커스터마이징 방법은 4가지 정도가 있다.
+class User(AbstractUser):
+    class GenderChoices(models.TextChoices):
+        MALE = "M", "남성"
+        FEMALE = "F", "여성"
+
+    gender = models.CharField(
+        verbose_name="성별",  # Admin 페이지에서 성별로 표시
+        max_length=1,  # 글자 길이 하나
+        choices=GenderChoices.choices,  # DB에는 M, F만 들어가고 Admin 페이지에서는 남성 여성으로 표시.
+    )
+
+    JOBS = (
+        ("P", "교수/강사(Professor/Lecturer)"),
+        ("S", "학생(Student)"),
+        ("R", "연구원(Researcher)"),
+        ("E", "기타(Etc.)"),
+    )
+
+    job = models.CharField(
+        verbose_name="직업",
+        max_length=1,
+        choices=JOBS,  # 튜플 형식으로도 지정할 수 있다.
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
---
+++ accounts/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+# Create your views here.
+
+
+# dev_9
+def login_user(request):
+
+    if request.method == "POST":
+        # username = request.POST.get("username", "") 이게 더 안전한 표현
+        username = request.POST["username"]
+        password = request.POST["password"]
+        """
+        POST 는 request 객체 안에 저장되어있는 딕셔너리 타입.
+        class HTTPRequest:
+            POST = {"username" : "admin", "password" : "1234"}
+        """
+        # DB에 있는지를 조회 후 대조
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Have been logged in")
+            return redirect("/")
+        else:
+            messages.success(request, ("There was an error, please try again"))
+            return redirect("accounts:login_user")  # /accounts/login
+    else:
+        return render(request, "accounts/login.html", {})
+
+
+def logout_user(request):
+    logout(request)
+    return redirect("/")
