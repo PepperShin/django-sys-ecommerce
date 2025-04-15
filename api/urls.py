4fbb035d123e8c0a04f6b45aff503a4bce5c9537
PepperShin <peep1996@naver.com>
2025-04-15 11:37:24 +0900

    commit

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
-from api.views import hello_world, hello_world_json, hello_world_drf
+
+# dev_29
+# 바꾼 views 파일들 한번에 끌어오기
+from .views import base_views, product_views
 
 # dev_28
 app_name = "api"
 
 urlpatterns = [
-    path("hello-world/", hello_world),
-    path("hello-world-json/", hello_world_json),
-    path("hello-world-drf/", hello_world_drf),
+    path("hello-world/", base_views.hello_world),
+    path("hello-world-json/", base_views.hello_world_json),
+    path("hello-world-drf/", base_views.hello_world_drf),
+    # dev_29
+    path("products/", product_views.products_api),
 ]
---
+++ api/views/product_views.py
@@ -0,0 +1,15 @@
+from rest_framework.decorators import api_view
+from store.models import Product
+from api.serializers import ProductSerializer
+from rest_framework.response import Response
+
+
+# dev_29
+@api_view(["GET"])
+def products_api(request):
+    if request.method == "GET":
+        products = Product.objects.all()
+        # many=True ➜ 여러 개의 인스턴스 (QuerySet, 리스트 등)
+        # many=False (기본값) ➜ 단일 인스턴스
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
